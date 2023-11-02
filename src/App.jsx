e346429d769b97dfcf8135e50260eb545f2d8947
Artem Faust <91615835+IronHedgehog@users.noreply.github.com>
2023-11-02 10:46:36 +0200

    component/profile-card

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import "./App.css";
+import Avatar from "./components/avatar/Avatar";
+import Intro from "./components/intro/Intro";
+import SkillList from "./components/skillList/SkillList";
+
+function App() {
+  return (
+    <div className="card">
+      <Avatar />
+      <div className="data">
+        <Intro />
+        {/* Should contain one Skill component
+        for each web dev skill that you have,
+        customized with props */}
+        <SkillList />
+      </div>
+    </div>
+  );
+}
+
+export default App;
